0927231bbdf1a2701969dac13cf9248c28cc42f7
hy <ttgml@outlook.com>
2025-02-21 12:00:25 +0800

    tmp

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.12.9-bookworm
+
+ADD requirements.txt /app/requirements.txt
+RUN pip install -r /app/requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple/
+COPY src /app/src
+WORKDIR /app/src
+CMD ["python", "-m", "funcproxy.cli"]
--- src/funcproxy/plugins/web_search/__init__.py
+++ src/funcproxy/plugins/web_search/__init__.py
@@ -1,12 +1,32 @@
 from funcproxy.core.plugin_base import PluginBase
 import json
+import requests
 
 class Plugin(PluginBase):
     def __init__(self):
         print("Web Search Plugin initialized")
+        self.firecrawl_api = "https://api.firecrawl.dev/v1/scrape"
 
     def do_search_online(self, parameters) -> str:
+        parameters = json.loads(parameters)
         return "not found"
     
     def do_open_website(self, parameters) -> str:
-        return "not found"
+        parameters = json.loads(parameters)
+        setting = self.get_settings()
+        try:
+            headers = {
+                "Authorization": "Bearer " + setting['current']["apikey"]
+            }
+            data = {
+                "url": parameters["url"],
+                "formats": ["markdown"]
+            }
+            response = requests.post(self.firecrawl_api, json=data, headers=headers)
+            if response.status_code == 200:
+                if response.json()['success'] == True:
+                    return response.json()["data"]["markdown"]
+            return "not found"
+        except Exception as e:
+            print(e)
+            return "not found"
